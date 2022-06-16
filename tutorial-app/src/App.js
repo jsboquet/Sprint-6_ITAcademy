9125bc1ca3755923d1b0017f2797fd5cae81a409
Joel <jsboquet@gmail.com>
2022-06-16 12:13:33 +0200

    Exercici 1

---
+++ tutorial-app/src/App.js
@@ -0,0 +1,11 @@
+import Escena from "./components/escena/Escena";
+
+function App() {
+  return (
+    <div className="App">
+      <Escena />
+    </div>
+  );
+}
+
+export default App;
---
+++ tutorial-app/src/components/escena/Escena.js
@@ -0,0 +1,8 @@
+function Escena() {
+  const text =
+    "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial";
+
+  return <p>{text}</p>;
+}
+
+export default Escena;
